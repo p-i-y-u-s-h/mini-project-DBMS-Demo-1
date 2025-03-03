e81351772ef3bed74fdf798451b87e97b838d58d
Piyush Mahajan <mahajanpiyush005@gmail.com>
2025-03-03 21:43:22 +0530

    commit-3

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+require("dotenv").config();
+
 const express = require("express");
+const { userRouter } = require("./routes/user");
 
 const app = express();
+
+app.use(express.json());
+app.use("/api/v1/user",userRouter);
+
+async function main(){
+    await mongoose.connect(process.env.MONGO_URL)
+    app.listen(3000)
+    console.log("hiuegenjhhon")
+}
 
 app.listen(3000);
